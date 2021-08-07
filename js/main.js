a2d1f3293de3d43036784469fb7eaad02cd479e6
Blaine Wood <blainewood@hotmail.com>
2021-08-07 16:41:25 -0600

    Adding audio source and main.js to integrate Pizzicato
    Adding nature.wav and restaurant.wav
    - These are tracked through git-lfs
    
    Adding main.js which defines interaction with Pizzicato
    - Currently depends on a ThreeBandEqualizer Effect which doesn't exist
    - Specified 3 sliders for controlling eq gains as effect parameters
    - Attached play, pause, and stop functionality
    - Connected nature.wav as the selected sound
    
    Updating index.html to match current main.js

---
+++ js/main.js
@@ -0,0 +1,91 @@
+// Effects
+/*
+var threeBandEq = new Pizzicato.Effects.ThreeBandEqualizer({
+    cutoff_frequency_low: 400,
+    cutoff_frequency_high: 4000,
+    low_band_gain: 1,
+    mid_band_gain: 1,
+    high_band_gain: 1,
+    peak: 10
+});
+*/
+
+// PZ Sound
+var sound = new Pizzicato.Sound('audio/nature.wav', function() {
+    // Enable after adding the threeBandEq to Pizzacato
+    //sounds.last().addEffect(threeBandEq);
+});
+
+
+var threeBandSegment = {
+    audio: sound,
+    playButton: document.getElementById('play'),
+    stopButton: document.getElementById('stop'),
+    lowBandSlider: document.getElementById('low-band-gain'),
+    midBandSlider: document.getElementById('mid-band-gain'),
+    highBandSlider: document.getElementById('high-band-gain'),
+    /*
+    effects: [
+	{
+	    instance: threeBandEq,
+	    parameters: {
+                // cutoff_frequency_low: threeBandEq.cutoff_fr3equency_low,
+                // cutoff_frequency_high: threeBandEq.cutoff_frequency_high,
+                low_band_gain: document.getElementById('low-band-gain'),
+                mid_band_gain: document.getElementById('mid-band-gain'),
+                high_band_gain: document.getElementById('high-band-gain'),
+		// peak: threeBandEq.cutoff_frequency_high
+	    }
+	}
+    ]
+    */
+};
+
+
+(function(segment) {
+
+    segment.audio.on('play', function() {
+	segment.playButton.classList.add('pause');
+    });
+
+    segment.audio.on('stop', function() {
+	segment.playButton.classList.remove('pause');
+    });
+
+    segment.audio.on('pause', function() {
+	segment.playButton.classList.remove('pause');
+    });
+
+    segment.playButton.addEventListener('click', function(e) {
+	if (segment.playButton.classList.contains('pause'))
+	    segment.audio.pause();
+	else
+	    segment.audio.play();
+    });
+
+    segment.stopButton.addEventListener('click', function(e) {
+	segment.audio.stop();
+    });
+
+    if (!segment.effects || !segment.effects.length)
+	return;
+
+    for (var i = 0; i < segment.effects.length; i++) {
+	var effect = segment.effects[i];
+
+	for (var key in effect.parameters) {
+	    (function(key, slider, instance){
+
+		var display = slider.parentNode.getElementsByClassName('slider-db')[0];
+
+		slider.addEventListener('input', function(e) {
+                    instance[key] = e.target.valueAsNumber;
+		    display.innerHTML = 20 * Math.log10(e.target.valueAsNumber);
+		});
+
+	    })(key, effect.parameters[key], effect.instance);	
+	}
+    }
+    
+})(threeBandSegment);
+
